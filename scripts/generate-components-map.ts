95014cec38a288d07448d4e42acee5adb9d4090e
iamdin <iamdinq@gmail.com>
2023-10-05 13:05:44 +0800

    feat: update export components for 0.8.0

--- scripts/generate-components-map.ts
+++ scripts/generate-components-map.ts
@@ -6,14 +6,30 @@ import { join, dirname } from "path";
 
 const prettierConfig = await prettier.resolveConfig(".");
 
-function extractComponentExports(fileContent?: string) {
-  return fileContent?.match(/(?<=export\s{).*(?=})/gm)?.flatMap((line) =>
+// converts `export { Toast, type ToastProps } from './toast' ...` to  ['Toast']
+const findAllComponentExports = (fileContent?: string) => {
+  if (!fileContent) return;
+
+  const cleanedContent = fileContent.replace(/(\r\n|\n|\r)/gm, " ");
+  const matches = cleanedContent.match(/export\s+{[^}]*}/gm);
+
+  if (!matches) return;
+
+  return matches.flatMap((line) =>
     line
+      .replace(/export\s+{/, "")
+      .replace(/}/, "")
       .split(",")
       .map((x) => x.trim())
-      .filter((x) => /^[A-Z]/.test(x)),
+      .filter((x) => !x.startsWith("type "))
+      .filter((x) => /^[A-Z]\w*/.test(x))
+      .map((x) => {
+        const asREG = /.*as\s+([A-Z].*)/;
+        const result = asREG.exec(x);
+        return result?.[1] || x;
+      }),
   );
-}
+};
 
 async function main() {
   const { resolvePath } = createResolver(import.meta.url);
@@ -29,7 +45,10 @@ async function main() {
         const fileContent = await readFile(join(component, "index.mjs"), {
           encoding: "utf8",
         }).catch(() => undefined);
-        return [component.split("/").pop(), extractComponentExports(fileContent)];
+        return [
+          component.split("/").pop(),
+          findAllComponentExports(fileContent),
+        ];
       }),
     ),
   );
