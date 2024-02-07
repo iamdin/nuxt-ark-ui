d9663c966c33abfe63856e7dce4efd27e1cc2fd7
iamdin <iamdinq@gmail.com>
2024-02-07 23:33:00 +0800

    pref: add component code

--- src/module.ts
+++ src/module.ts
@@ -1,9 +1,5 @@
-import {
-  defineNuxtModule,
-  createResolver,
-  addComponent,
-} from "@nuxt/kit";
-import ArkComponents from "./components";
+import { addComponent, createResolver, defineNuxtModule } from "@nuxt/kit";
+import * as fs from "node:fs";
 import { dirname, join } from "path";
 
 // Module options TypeScript interface definition
@@ -28,20 +24,29 @@ export default defineNuxtModule<ModuleOptions>({
     prefix: "Ark",
   },
   async setup(options) {
-    const resolver  = createResolver(import.meta.url);
+    const resolver = createResolver(import.meta.url);
 
     const entrypoint = await resolver.resolvePath("@ark-ui/vue"); // node_modules/@ark-ui/vue/index.mjs
     const root = dirname(entrypoint); // node_modules/@ark-ui/vue
 
-    for (const key of objectKeys(ArkComponents)) {
-      for (const component of ArkComponents[key]) {
-        addComponent({
-          name: `${options.prefix}${component}`,
-          export: component,
-          filePath: join(root, key),
-          // chunkName: group.chunkName,
-          // mode: 'all'
-        });
+    // get export from index.mjs
+    const content = fs.readFileSync(entrypoint, "utf-8");
+
+    const cleanedContent = content.replace(/(\r\n|\n|\r)/gm, " ");
+    const matches =
+      cleanedContent.match(/export\s+{([^}]*)}\sfrom\s'([^;]*)'/gm) || [];
+
+    for (const match of matches) {
+      const [_, component, p] =
+        match.match(/export\s+{\s+([^}]*)\s+}\s+from\s+'([^;]*)'/) || [];
+      if (component && p) {
+        if (/^[A-Z]\w*/.test(component)) {
+          addComponent({
+            name: `${options.prefix}${component}`,
+            export: component,
+            filePath: join(root, p),
+          });
+        }
       }
     }
   },
